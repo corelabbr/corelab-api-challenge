38c0115c400a5c9e92bac9ae21826726b89e7359
Diener Dornelas <diener.ld@outlook.com>
2022-07-05 14:35:49 -0300

    create use case listall vehicles

---
+++ src/application/vehicle/list-all/listAll.usecase.ts
@@ -0,0 +1,10 @@
+import { IVehicle, Vehicle } from '@domain/entities/vehicle.entity';
+import { IVehicleRepository } from '@domain/interfaces/vehicle.repository';
+
+export class ListAllVehiclesUseCase {
+  constructor(private vehicleRepo: IVehicleRepository) {}
+
+  async execute(): Promise<{ total: number; data: IVehicle[] }> {
+    return this.vehicleRepo.findAll();
+  }
+}
