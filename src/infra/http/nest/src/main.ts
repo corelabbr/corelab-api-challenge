e374b5da0f10d688de0f0ae31a13a98f0a5f9884
Diener Dornelas <diener.ld@outlook.com>
2022-07-05 13:50:36 -0300

    init project

---
+++ src/infra/http/nest/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  const HOST = process.env.HOST || 'localhost';
+  await app.listen(PORT, () =>
+    console.log(`listening on host ${HOST}:${PORT}`),
+  );
+}
+bootstrap();
