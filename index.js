04f6de11b7307333e2f24abab1e7065bfa78bf2b
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-05 19:48:18 -0300

    Connect DataBase

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-const useRoutes = require('./Routes')
+require('dotenv').config()
+
+const useRoutes = require('./Routes/RouteCars')
 
 app.use(useRoutes)
+
+console.log(process.env.URL_MONGO)
+
+mongoose.connect(process.env.URL_MONGO)
+    .then(()=> console.log('Connected'))
+    .catch((err)=> console.log('Error', err))
 
 app.listen(3000, ()=> { console.log('Server Running') })
