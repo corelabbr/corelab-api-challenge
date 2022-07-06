6e5c838ffd94f380908407cee45013bf4af5781d
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-06 20:51:15 -0300

    Many Updates

--- Routes/RouteCars.js
+++ Routes/RouteCars.js
@@ -3,9 +3,20 @@ const router = express.Router()
 
 const Controllers = require('../Controllers/ControllerCars')
 
+// ----------------GET---------------- //
 router.get('/cars', Controllers.showCars)
+
+
+// ----------------POST---------------- //
 router.post('/new-car', express.json(), Controllers.newCar)
+
+
+// ----------------DEL---------------- //
 router.delete('/del-car', express.json(), Controllers.deleteCar)
+
+
+// ----------------PUT---------------- //
 router.put('/edit-car', express.json(), Controllers.editCar)
+
 
 module.exports = router
