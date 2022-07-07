b2d92dbb6f4259a6a996600a47852b5259432942
Diener Dornelas <diener.ld@outlook.com>
2022-07-06 22:10:12 -0300

    add cors

--- src/infra/http/nest/src/main.ts
+++ src/infra/http/nest/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: '*',
+  });
   const PORT = process.env.PORT || 3000;
   const HOST = process.env.HOST || 'localhost';
   await app.listen(PORT, () =>
