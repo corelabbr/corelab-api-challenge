5ed529138169bd85404a04b8b728e340edb2924c
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-10 23:48:00 -0300

    feat: create demo users when running for the first time

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
 import express, { json } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import vehiculeAdRoute from './routes/vehiculeAdRoute.js';
+import demoUsers from './middlewares/demoUsersMiddleware.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+demoUsers();
 app.use('/vehicule', vehiculeAdRoute);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("server is running port " + process.env.PORT )
+});
