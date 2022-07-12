bb374e6c43c799ec500ea8b13fd323cc5b797476
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-11 23:57:48 -0300

    feat: save with color code

--- src/controllers/vehiculeAdController.js
+++ src/controllers/vehiculeAdController.js
@@ -2,10 +2,24 @@ import db from '../databases/mongo.js';
 
 async function createAd(req, res) {
   const ad = res.locals.ad;
-  console.log(ad);
+  let colorCode;
+  switch (ad.color) {
+    case "Vermelho":
+      return colorCode = "red";
+    case "Cinza":
+      return colorCode = "gray";
+  }
+  const adObj = {
+    ...ad,
+    color: {
+      code: colorCode,
+      name: ad.color,
+    }
+  }
+  console.log(adObj);
   try {
-    await db.collection('ads').insertOne(ad);
-    res.status(201).send([{text: "Anúncio criado com sucesso", label: "success"}])
+    await db.collection('ads').insertOne(adObj);
+    res.status(201).send([{ text: "Anúncio criado com sucesso", label: "success" }])
   } catch (err) {
     res.status(500).send('createVehicule: \n' + err);
   }
@@ -13,7 +27,7 @@ async function createAd(req, res) {
 
 async function getAds(req, res) {
   try {
-    const vehicules = await db.collection('vehicules').find().toArray();
+    const vehicules = await db.collection('ads').find().toArray();
     res.status(200).send(vehicules);
   } catch (err) {
     res.status(500);
