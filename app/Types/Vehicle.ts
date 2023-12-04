328ccdd58a9649347437137957272e6b1c2f240f
Vitor Sponhardi <vitor.sponhardi@gmail.com>
2023-12-04 19:31:33 -0300

    Add files via upload

---
+++ app/Types/Vehicle.ts
@@ -0,0 +1,11 @@
+export interface IVehicle {
+    id: number;
+    name: string;
+    description: string;
+    plate: string;
+    isFavorite: boolean;
+    year: number;
+    color: string;
+    price: number;
+    createdAt: Date;
+}
