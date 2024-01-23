4a664ff4af1ee4f048831c164d1ace29910fa213
Oivlisfriend <robertomarcelogongo@gmail.com>
2024-01-23 14:51:18 +0100

    O ficheiro task na pasta models, foi configurado de acordo os atributos que terá de tratar e passado de js para ts

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,27 +1,55 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+import { DataTypes, Model } from 'sequelize';
+import connection from '../database/index';
+
+interface taskAttributes {
+  id?: number;
+  title?: string;
+  description?: string;
+  isYellowStar?: boolean;
+  isFavorite?: boolean;
+  color?: string;
+
+}
+
+class Task extends Model implements taskAttributes {
+  id?: number;
+  title?: string;
+  description?: string;
+  isYellowStar?: boolean;
+  isFavorite?: boolean;
+  color?: string;
+
+}
+Task.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.BIGINT
+  },
+  title: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  isYellowStar: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN
+  },
+  isFavorite: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN
+  },
+  color: {
+    allowNull: false,
+    type: DataTypes.STRING
   }
-  task.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    isYellowStar: DataTypes.BOOLEAN,
-    isFavorite: DataTypes.BOOLEAN,
-    color: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'task',
-  });
-  return task;
-};
+}, {
+  sequelize: connection,
+  underscored: false
+})
+
+module.exports = Task;
