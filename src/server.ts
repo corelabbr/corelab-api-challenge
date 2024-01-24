2b0ff43d7fc76cbf9757de41ea6bb4df0e50d0e7
thevalter <valternunes776@gmail.com>
2024-01-23 21:04:35 -0300

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import Express from "express";
+import express from "express";
+import cors from "cors"
 import NoteController from "./adapters/NoteController/NoteController";
 import NoteService from "./core/notes/service/NoteService";
-import NoteRepositoryPrismaPg from "./external/prisma/NoteRepositoryPrismaPg"
-const app = Express();
-app.use(Express.json());
+import NoteRepositoryPrismaPg from "./external/prisma/NoteRepositoryPrismaPg";
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
@@ -12,3 +14,5 @@ const note = new NoteService(noteRepositoryPrismaPg);
 new NoteController(app, note);
 
 app.listen(PORT, () => console.log(`app runnning on port ${PORT}`));
+
+export default app;
