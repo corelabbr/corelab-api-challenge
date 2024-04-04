82b9dc40c2b7248515afc9ce1cb7eb51fea5a5f6
Ruan Gomes <ruangomesdorosario@gmail.com>
2024-04-03 22:14:34 -0300

    Adiciona controller de Task em routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import TasksController from '../app/controllers/tasks_controller.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.resource('/api/tasks', TasksController)
