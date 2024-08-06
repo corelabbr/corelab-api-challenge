921f6b4c600e0970ef4641266550b3e6aee5b924
Davi Batista <davideosmar11@gmail.com>
2024-08-06 14:56:38 -0300

    feat: added favorite note repository unit tests

--- src/modules/favorite/repository/favorite.repository.spec.ts
+++ src/modules/favorite/repository/favorite.repository.spec.ts
@@ -1,7 +1,81 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
 import { FavoriteRepository } from './favorite.repository';
+import { Favorite } from '../entity/favorite.entity';
 
-describe('FavoriteRepository', () => {
-  it('should be defined', () => {
-    expect(new FavoriteRepository()).toBeDefined();
+describe('Note Repository Test Suites', () => {
+  let favoriteNoteRepository: FavoriteRepository;
+
+  const note = new Favorite(
+    1,
+    1
+  )
+
+  const dataSource = {
+    createEntityManager: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest.useFakeTimers({ doNotFake: ['nextTick'] });
   });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FavoriteRepository,
+        {
+          provide: DataSource,
+          useValue: dataSource,
+        },
+      ],
+    }).compile();
+
+    favoriteNoteRepository = module.get<FavoriteRepository>(FavoriteRepository);
+  });
+
+  it('should not find a note by its id using the getById passing an invalid id', async () => {
+    const findOneSpy = jest
+      .spyOn(favoriteNoteRepository, 'findOne')
+      .mockResolvedValue(null);
+
+    const foundNote = await favoriteNoteRepository.findFavoriteNote(0, 0);
+
+    expect(foundNote).toBe(null);
+  });
+
+  it('should find a note by its id using the getById passing a valid id', async () => {
+    const findOneSpy = jest
+      .spyOn(favoriteNoteRepository, 'findOne')
+      .mockResolvedValue(note);
+
+    const foundNote = await favoriteNoteRepository.findFavoriteNote(1, 1);
+
+    expect(foundNote).toBe(note);
+  });
+
+  it('should not find all the favorite notes passing an invalid user_id', async ()  =>  {
+    const findSpy = jest
+      .spyOn(favoriteNoteRepository, 'find')
+      .mockResolvedValue([]);
+
+    const foundNotes = await favoriteNoteRepository.findUserFavoriteNotes(0);
+
+    expect(foundNotes).toEqual([]);
+  })
+
+  it('should find all the fav notes that belong to the user', async () => {
+    const findSpy = jest
+      .spyOn(favoriteNoteRepository, 'find')
+      .mockResolvedValue([note]);
+
+    const foundNotes = await favoriteNoteRepository.findUserFavoriteNotes(1);
+
+    expect(foundNotes).toEqual([note]);
+  });
+
+  
 });
