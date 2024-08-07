b7bc69b7a18edf44ecba70db2bb13ef144f94a6f
rodi38 <rodrigorochaed1@gmail.com>
2024-08-07 14:54:21 -0300

    feat: Entity Task

--- src/modules/task/entities/Task.entity.ts
+++ src/modules/task/entities/Task.entity.ts
@@ -1,4 +1,26 @@
-//todo
+import { ObjectId } from "mongodb";
+import { Column, CreateDateColumn, Entity, ObjectIdColumn, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('tasks')
 class Task {
+    @ObjectIdColumn()
+    _id!: ObjectId;
+
+    @Column({type: 'text'})
+    title!: string;
+
+    @Column({type: 'text'})
+    taskContent!: string;
+
+    @Column({type: 'boolean', default: false})
+    isFavorite!: boolean;
+    
+    @Column({type: 'timestamp'})
+    createdAt: Date;
+
+    @Column({type: 'timestamp'})
+    updatedAt: Date;
 
 }
+
+export { Task };
