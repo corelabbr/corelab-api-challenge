381e480ee7180efb7666142688cef20ad8af0701
rodi38 <rodrigorochaed1@gmail.com>
2024-08-07 14:48:04 -0300

    feat: ResponseTaskDto

---
+++ src/modules/task/dtos/ResponseTaskDto.ts
@@ -0,0 +1,25 @@
+//todo
+
+import { ObjectId } from "mongodb"
+import { Task } from "../entities/Task.entity"
+
+class ResponseTaskDto {
+  _id: ObjectId;
+
+  title: string;
+
+  taskContent: string;
+
+  isFavorite: boolean;
+
+  constructor(task: Task) {
+    this._id = task._id
+    this.title = task.title
+    this.taskContent = task.taskContent
+    this.isFavorite = task.isFavorite
+
+  }
+
+}
+
+export { ResponseTaskDto }
