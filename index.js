b4274c26d618570adbcf5a83d838a31622ec50e6
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-08-07 12:57:09 -0300

    Adicionar endpoints para buscar todas as notas e para criar uma nota

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const express = require("express")
+import express from "express"
+import { PrismaClient } from "@prisma/client"
+
 const app = express()
 const port = 3000
+const prisma = new PrismaClient()
+
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   res.send("hello world")
+})
+
+app.get("/todas", async (req, res) => {
+  let todasNotas = await prisma.notas.findMany()
+  res.send(todasNotas)
+})
+
+app.post("/criar", async (req, res) => {
+  let nota = req.body
+  await prisma.notas.create({
+    data: {
+      titulo: nota["titulo"],
+      conteudo: nota["conteudo"],
+    }
+  })
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
