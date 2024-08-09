3f1908f90c2843ef013f34e35d8e2a5953b4ce1a
rodi38 <rodrigorochaed1@gmail.com>
2024-08-08 23:00:04 -0300

    feat: routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,1 +1,8 @@
-//todo
+import { Router } from "express";
+import { taskRoutes } from "modules/task/infra/http/task.routes";
+
+const router = Router();
+
+router.use('/tasks', taskRoutes);
+
+export default router;
