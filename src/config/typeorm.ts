a043db425444e38dc83711e3d76581f19e1ad4d1
rodi38 <rodrigorochaed1@gmail.com>
2024-08-08 21:57:34 -0300

    feat: typeorm config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,1 +1,15 @@
-//todo
+import 'reflect-metadata';
+import { DataSource } from "typeorm";
+import { loadEnv } from './env.config';
+import { Task } from 'modules/task/entities/Task.entity';
+
+loadEnv()
+export const AppDataSource = new DataSource({
+  type: 'mongodb',
+  url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+  database: `${process.env.MONGO_DATABASE}`,
+  authSource: `${process.env.MONGO_USER}`,
+  entities: [Task],
+  logging: false,
+  directConnection: true,
+});
