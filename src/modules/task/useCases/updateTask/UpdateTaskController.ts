e501e20978b1d2edd1a7e79918607b2de3572fff
rodi38 <rodrigorochaed1@gmail.com>
2024-08-08 22:37:23 -0300

    feat: UpdateTask controller

---
+++ src/modules/task/useCases/updateTask/UpdateTaskController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container} from "tsyringe";
+import { UpdateTaskService } from "./UpdateTaskService";
+import { ICreateTaskDto } from "modules/task/dtos/ICreateTaskDto";
+
+class UpdateTaskController {
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const {title, taskContent, isFavorite}: ICreateTaskDto = request.body;
+
+    const getTaskByIdController = container.resolve( UpdateTaskService );
+    const task = await getTaskByIdController.execute(id, {title, taskContent, isFavorite});
+
+    
+    return response.status(200).send(task);
+   }
+}
+
+export { UpdateTaskController }
