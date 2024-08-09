eabc96e544517f16d560c506f86c9ab92c0f957c
rodi38 <rodrigorochaed1@gmail.com>
2024-08-08 22:15:58 -0300

    feat: CreateTask controller

--- src/modules/task/useCases/createTask/CreateTaskController.ts
+++ src/modules/task/useCases/createTask/CreateTaskController.ts
@@ -7,12 +7,12 @@ import { Request, Response } from "express";
 class CreateTaskController {
 
   async handle(request: Request, response: Response) : Promise<Response> {
+    const { title, taskContent, isFavorite }: ICreateTaskDto = request.body;
+    
+    const createTaskService = container.resolve(CreateTaskService);
 
-    const { title, taskContent }: ICreateTaskDto = request.body;
-    const createUserService = container.resolve(CreateTaskService);
-
-    const task = await createUserService.execute({
-      title, taskContent
+    const task = await createTaskService.execute({
+      title, taskContent, isFavorite
     });
 
     return response.status(201).send(task);
