5d926d32b01ef051fdd3f2aac5758f0defa91b83
Mateus Ávila <mateus.avila777@gmail.com>
2024-08-09 12:14:19 -0300

    Adding interfaces

---
+++ src/interfaces/ITask.ts
@@ -0,0 +1,11 @@
+import { Task } from '@/entities/task'
+
+export interface ITask {
+    create(data: Task): Promise<Task>
+    editTask(id: string, title: string, content: string, color: string | 'white' | 'blue' | 'green' | 'yellow' | 'salmon' | 'red' | 'blue2' | 'pink' | 'lemon' | 'orange' | 'gray' | 'gray2' | 'brown'): Promise<Task>
+    setFavorite(id: string, favorite: boolean): Promise<Task>
+    getTasks(): Promise<Task[] | null>
+    getFavoriteTasks(): Promise<Task[] | null>
+    findTask(id: string): Promise<Task | null>
+    deleteTask(id: string): Promise<object | null>
+}
