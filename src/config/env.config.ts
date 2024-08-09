2df1ab2eb3ae74d65f3c0e6a2fb3def1862b82ec
rodi38 <rodrigorochaed1@gmail.com>
2024-08-08 21:55:04 -0300

    chore: env config and package

---
+++ src/config/env.config.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+
+
+export function loadEnv(): void {
+  let path: string;
+
+  switch(process.env.NODE_ENV) {
+    case 'test':
+      path = '.env.test';
+      break
+
+    case 'dev':
+      path = '.env.dev';
+      break
+      
+    default:
+        path = '.env'
+  }
+
+  const currentEvents = dotenv.config({path});
+
+  if(currentEvents.error) throw new Error(`failed to load env from ${path}: ${currentEvents.error}`);
+
+  dotenvExpand.expand(currentEvents);
+}
