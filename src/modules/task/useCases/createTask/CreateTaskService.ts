499fc31122f035dcbc5c51b155d6f12968bd3a00
rodi38 <rodrigorochaed1@gmail.com>
2024-08-10 10:23:16 -0300

    fix: CreateTask validation

--- src/modules/task/useCases/createTask/CreateTaskService.ts
+++ src/modules/task/useCases/createTask/CreateTaskService.ts
@@ -1,11 +1,9 @@
-
-
-
 import { inject, injectable } from "tsyringe";
 import { ITaskRepository } from "../../repositories/ITaskRepository";
-import { AppError } from "../../../../shared/infra/http/errors/AppError";
 import { ResponseTaskDto } from "../../dtos/ResponseTaskDto";
 import { ICreateTaskDto } from "modules/task/dtos/ICreateTaskDto";
+import { AppError } from "shared/infra/http/errors/AppError";
+import { hasEmptyValues } from "utils/util";
 
 @injectable()
 class CreateTaskService{
@@ -15,7 +13,7 @@ class CreateTaskService{
   ){}
 
   async execute(data: ICreateTaskDto):Promise<ResponseTaskDto>{
- 
+    if(hasEmptyValues(data)) throw new AppError("Task must have title and content!", 400)
     const task = await this.taskRepository.save(data)
   
     return new ResponseTaskDto(task);
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,10 @@
+import { ICreateTaskDto } from "modules/task/dtos/ICreateTaskDto";
+
 export default function stringToBoolean(str: string | boolean): boolean {
   return str === "true"? true : str === "false" ? false : undefined;
 }
+
+export const hasEmptyValues = (obj: ICreateTaskDto) => {
+  const { title, taskContent} = obj;
+  return Object.values({title, taskContent}).some((value) => value === null || value === undefined || value === "");
+};
