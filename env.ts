03d2e294eea3f3d03ffb37d6a2698a9fcacd4bfb
Luiz Livio <livio44@gmail.com>
2025-05-30 01:01:40 -0300

    Elaboração do projeto - Corelab Notes

--- env.ts
+++ env.ts
@@ -21,4 +21,5 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  MONGO_URL: Env.schema.string.optional({ format: 'url' }),
 })
