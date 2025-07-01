120f49367afca9b2d98b2ab5cbdbecf3d4aea5f6
Iury Veloso <iurysveloso@gmail.com>
2025-07-01 17:31:48 -0300

    feat: docker

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,9 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
-import Note from './note.js'
-import { type HasMany } from '@adonisjs/lucid/types/relations'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
